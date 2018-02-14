28c6ad5826b82a0c4914d89bdf9382b5b6d45639
Rudolf Polzer <divVerent@xonotic.org>
2018-02-14 09:37:04 -0800

    Make sure that the saver's process group gets killed when the main process dies, too.

--- saver_child.c
+++ saver_child.c
@@ -28,18 +28,23 @@ static pid_t saver_child_pid = 0;
 
 void WatchSaverChild(Display* dpy, Window w, const char* executable,
                      int should_be_running) {
-  if (!should_be_running && saver_child_pid != 0) {
-    // Kill the whole process group.
-    kill(saver_child_pid, SIGTERM);
-    kill(-saver_child_pid, SIGTERM);
-    while (saver_child_pid) {
+  if (saver_child_pid != 0) {
+    if (!should_be_running) {
+      // Kill the whole process group.
+      kill(saver_child_pid, SIGTERM);
+      kill(-saver_child_pid, SIGTERM);
+    }
+    do {
       int status;
-      pid_t pid = waitpid(saver_child_pid, &status, 0);
+      pid_t pid =
+          waitpid(saver_child_pid, &status, should_be_running ? WNOHANG : 0);
       if (pid < 0) {
         switch (errno) {
           case ECHILD:
             // The process is dead. Fine.
+            kill(-saver_child_pid, SIGTERM);
             saver_child_pid = 0;
+            XClearWindow(dpy, w);
             break;
           case EINTR:
             // Waitpid was interrupted. Need to retry.
@@ -51,20 +56,27 @@ void WatchSaverChild(Display* dpy, Window w, const char* executable,
         }
       } else if (pid == saver_child_pid) {
         if (WIFEXITED(status)) {
-          // The process did exit.
+          // Auth child exited.
+          if (should_be_running) {
+            // To be sure, let's also kill its process group before we restart
+            // it (no need to do this if we already did above).
+            kill(-saver_child_pid, SIGTERM);
+          }
+          saver_child_pid = 0;
           if (WEXITSTATUS(status) != EXIT_SUCCESS) {
             fprintf(stderr, "Saver child failed with status %d.\n",
                     WEXITSTATUS(status));
           }
-          saver_child_pid = 0;
           // Now is the time to remove anything the child may have displayed.
           XClearWindow(dpy, w);
         }
         // Otherwise it was suspended or whatever. We need to keep waiting.
+      } else if (pid == 0 && should_be_running) {
+        // We're still alive.
       } else {
         fprintf(stderr, "Unexpectedly woke up for PID %d.\n", (int)pid);
       }
-    }
+    } while (!should_be_running && saver_child_pid != 0);
   }
 
   if (should_be_running && saver_child_pid == 0) {
