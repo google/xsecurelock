ce04e674d5ae9e2dc69bb102be2896bfc6b8f910
Rudolf Polzer <divVerent@xonotic.org>
2018-02-21 11:22:18 -0800

    Don't run tests with a Xinerama setup because Xephyr does it wrong (root window instantly resizes to the first screen, and Xrandr info is incomplete).

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -4,5 +4,5 @@ for test in *.xdo; do
   startx \
     /bin/sh "$PWD"/run-test.sh "$test" \
     -- \
-    "$(which Xephyr)" :42 -retro -screen 800x480 -screen 1280x720 +xinerama
+    "$(which Xephyr)" :42 -retro -screen 640x480
 done
