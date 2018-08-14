6d61a1bea3b5cbb6f53df29d397e0f4739aa5792
Rudolf Polzer <divVerent@xonotic.org>
2018-08-14 10:35:18 -0700

    Log the PID and time with every log line.

--- logging.c
+++ logging.c
@@ -3,11 +3,24 @@
 #include <errno.h>   // for errno
 #include <stdarg.h>  // for va_end, va_list, va_start
 #include <stdio.h>   // for fputs, stderr, vfprintf, perror, NULL
+#include <time.h>
+#include <unistd.h>
+
+static void PrintLogPrefix(void) {
+  time_t t = time(NULL);
+  struct tm tm_buf;
+  struct tm *tm = gmtime_r(&t, &tm_buf);
+  char s[32];
+  if (!strftime(s, sizeof(s), "%Y-%m-%dT%H:%M:%SZ ", tm)) {
+    *s = 0;
+  }
+  fprintf(stderr, "%s%ld xsecurelock: ", s, (long)getpid());
+}
 
 void Log(const char *format, ...) {
   va_list args;
   va_start(args, format);
-  fputs("xsecurelock: ", stderr);
+  PrintLogPrefix();
   vfprintf(stderr, format, args);
   fputs(".\n", stderr);
   va_end(args);
@@ -17,7 +30,7 @@ void LogErrno(const char *format, ...) {
   int errno_save = errno;
   va_list args;
   va_start(args, format);
-  fputs("xsecurelock: ", stderr);
+  PrintLogPrefix();
   vfprintf(stderr, format, args);
   fputs(": ", stderr);
   errno = errno_save;
