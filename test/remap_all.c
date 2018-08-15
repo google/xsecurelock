53c2005e29016ae6bb6e78c377c1f1668a7b063c
Rudolf Polzer <divVerent@xonotic.org>
2018-08-15 06:32:48 -0700

    Compile but don't install the test tools like remap_all.
    I need it for my Bspwm setup ;)

--- test/remap_all.c
+++ test/remap_all.c
@@ -2,6 +2,7 @@
 
 #include <X11/Xlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
   Display *display = XOpenDisplay(NULL);
@@ -12,7 +13,7 @@ int main(int argc, char **argv) {
   UnmapAllWindowsState state;
   XGrabServer(display);
   InitUnmapAllWindowsState(&state, display, DefaultRootWindow(display), NULL, 0,
-                           NULL, NULL, 0);
+                           NULL, NULL, argc > 1 ? atoi(argv[1]) : 0);
   UnmapAllWindows(&state);
   RemapAllWindows(&state);
   XUngrabServer(display);
